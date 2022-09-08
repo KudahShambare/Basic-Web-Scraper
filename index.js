507b19f1aad7ba21fdf225d10108760249eb37db
Kudakwashe Shambare <kudahshambare263@gmail.com>
2022-09-08 23:04:02 +0200

    template created

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+import fetch from "node-fetch";
+import cheerio from "cheerio";
+
+
+const url="https://www.pindula.co.zw/Joshua_Maponga"
+
+const scrap= async()=>{
+	try{
+		const feching =await fetch(url);
+		console.log(feching.body)
+	const cheerioLoad =cheerio.load(feching);
+const result = cheerioLoad("p").text();
+console.log(result)
+
+}
+catch(err){
+	console.error(err)
+}
+}
+scrap();
