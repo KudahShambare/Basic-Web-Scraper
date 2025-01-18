219fadc9e538e502fc0cf1f827bbcf275c51d8b1
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2025-01-18 12:14:10 +0200

    Add data scraping steps and update URL handling in the application

--- index.js
+++ index.js
@@ -1,75 +1,69 @@
-
-
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
+import read from "node-readability";
 
+const app = express();
 
-import { duckSearch } from "./searchEngine.js";
-
-
-const app=express();
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //use path to access fies
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.static("frontend"));
 
+/**   Fuctions */
 
-app.get("/data",(req,resp)=>{
-	
-duckSearch("Mugabe").then(val =>{
-		console.log(val);
+const objectifyBlog =  (url) => {
+  return new Promise((resolve, reject) => {
+    let obj = {
+      title: "",
+      textBody: "",
+    };
+    read(url, function (err, article, meta) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
 
-		resp.json(val)
-		
-	})
-})
+      // Title
+      let title = article.title;
 
+      // HTML Text
+      let textBody = article.textBody;
+    
+      obj.title = title;
+      obj.textBody = textBody;
+      }
 
-app.post("/url",(req,resp)=>{
-
-	const url=req.body.url;
-
-
-let p= scrap(); //returns a promise which has to be resolved by the then function
-p.then((a)=>{
-	
-
-	const fileHead=a[0];  // first h1 which in most cases the heading of the blog
-
-
-	a.pop(); // remove last paragraph which usually has blog meta details
-
-
-
-
-	resp.set({
-		"Content-Type":"applicatio/pdf",
-		"Content-Disposition":"attachment/output.pdf"
-	}).redirect("/get-file")
-
-})
+      // Close article to clean up
+      article.close();
+      resolve(obj);
+    });
+  });
+}
 
 
+/*****Route */
+app.post("/url", async (req, resp) => {
+  const urlArr = req.body.url;
 
-})
+  const promises = urlArr.map((url) => objectifyBlog(url));
+  const blogArr = await Promise.all(promises);
 
-app.get("/get-file",(req,resp)=>{
-	resp.sendFile(__dirname+"/output.pdf")
-})
+  // Introduce a delay if needed
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  //console.log(blogArr);
+  resp.json(blogArr);
+});
+
+app.get("/get-file", (req, resp) => {
+  resp.sendFile(__dirname + "/output.pdf");
+});
 
 const PORT = 5500;
-app.listen(PORT,()=>{
-	console.log("App listening to port "+PORT);
-})
-
-//webpage 
-
-
-
-//sortable plainrowheaders wikitable jquery-tablesorter
-
+app.listen(PORT, () => {
+  console.log("App listening to port " + PORT);
+});
---
+++ steps.md
@@ -0,0 +1,3 @@
+1. Scrap data from a url
+2. Clean the scrapped data (regex)
+3. Summarize the text (LLM/NLP model)
